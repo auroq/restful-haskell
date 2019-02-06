39037921c588fe3099d0e9a6972119cf3c801267
Parker Johansen <johansen.parker@gmail.com>
2019-02-06 00:58:46 -0700

    First dummy API

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,42 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( runDaemon
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Web.Spock
+import           Web.Spock.Config
+
+import           Data.Aeson       hiding (json)
+import           Data.Monoid      ((<>))
+import           Data.Text        (Text, pack)
+import           GHC.Generics
+
+data Person = Person
+  { name :: Text
+  , age  :: Int
+  } deriving (Generic, Show)
+
+instance ToJSON Person
+
+instance FromJSON Person
+
+type Api = SpockM () () () ()
+
+type ApiAction a = SpockAction () () () a
+
+runDaemon :: IO ()
+runDaemon = do
+  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  runSpock 8080 (spock spockCfg app)
+
+app :: Api
+app = do
+  get "people" $ do
+    json [Person { name = "Fry", age = 25 }, Person { name = "Bender", age = 4 }]
+
+  post "people" $ do
+    thePerson <- jsonBody' :: ApiAction Person
+    text $ "Parsed: " <> pack (show thePerson)
+
